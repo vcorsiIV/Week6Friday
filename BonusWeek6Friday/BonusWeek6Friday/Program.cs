6e83d9dd004891b89a224f9e1cbaecadfdc9447f
vcorsiIV <S01160644@acad.tri-c.edu>
2017-02-24 20:30:03 -0500

    Bonus Assignment
    The bonus assignment for week 6.

---
+++ BonusWeek6Friday/BonusWeek6Friday/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BonusWeek6Friday
+{
+    class Bonus
+    {
+        string strInt;
+        int userInt;
+
+        public void userInput()
+        {
+            Write("Enter in a number to display a multiplication table:\t");
+            strInt = Console.ReadLine();
+        }
+
+        public void mulTable()
+        {
+            int[] intArr = new int[11];
+            userInt = int.Parse(strInt);
+            for (int i = 0; i < 11; i++)
+            {
+                int prod = userInt* i;
+                WriteLine(userInt + " * " + i +" = " + prod);
+                
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int[] retedTable = new int[11];
+                Bonus mt = new Bonus();
+                mt.userInput();
+                mt.mulTable();
+            }
+        }
+    }
+}
